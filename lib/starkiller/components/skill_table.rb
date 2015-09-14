060e2547447d9fbcd6b83376abd3b6ca56eaa729
sparker <scott.p.parker@gmail.com>
2015-09-14 12:32:49 -0500

    Skill table now prints

--- lib/starkiller/components/skill_table.rb
+++ lib/starkiller/components/skill_table.rb
@@ -14,6 +14,7 @@ module Starkiller
       LABEL_COLOR = "aaaaaa"
 
       PROFICIENCY_SCALE = 3.0
+      PROFICIENCY_BASE_WIDTH = 2
       PROFICIENCY_POLYGON_POINTS = [
         [0.5, 0],
         [1.5, 0],
@@ -22,6 +23,17 @@ module Starkiller
         [0.5, 0 - Math.sqrt(3)],
         [0, 0 - Math.sqrt(3) / 2.0]
       ]
+
+      ABILITY_SCALE = 3.0
+      ABILITY_BASE_WIDTH = 1
+      ABILITY_POLYGON_POINTS = [
+        [0.5, 0],
+        [1, 0 - Math.sqrt(3) / 2.0],
+        [0.5, 0 - Math.sqrt(3)],
+        [0, 0 - Math.sqrt(3) / 2.0]
+      ]
+
+      DIE_BUFFER = 0.7
 
       class << self
         def render(pdf, character)
@@ -167,9 +179,36 @@ module Starkiller
             font: FONT_MINOR_TEXT
           )
 
-          pdf.polygon(*PROFICIENCY_POLYGON_POINTS.map { |x,y = a| [ SKILL_ROLL_LEFT + PROFICIENCY_SCALE * x, vertical_offset - 0.5 + PROFICIENCY_SCALE * y] })
-          pdf.fill_color "ffff00"
-          pdf.fill_and_stroke
+          draw_dice(pdf,
+            top: vertical_offset - 0.5,
+            right: full_width - 2,
+            proficiency: (skill[:proficiency] || 0).to_i,
+            ability: skill[:ability].to_i
+          )
+        end
+
+        def draw_dice(pdf, opts)
+          horizontal_offset = opts[:right]
+
+          opts[:proficiency].times do
+            horizontal_offset -= PROFICIENCY_BASE_WIDTH * PROFICIENCY_SCALE
+
+            pdf.polygon(*PROFICIENCY_POLYGON_POINTS.map { |x,y = a| [ horizontal_offset + PROFICIENCY_SCALE * x, opts[:top] + PROFICIENCY_SCALE * y] })
+            pdf.fill_color "ffff00"
+            pdf.fill_and_stroke
+
+            horizontal_offset -= DIE_BUFFER
+          end
+
+          opts[:ability].times do
+            horizontal_offset -= ABILITY_BASE_WIDTH * ABILITY_SCALE
+
+            pdf.polygon(*ABILITY_POLYGON_POINTS.map { |x,y = a| [ horizontal_offset + ABILITY_SCALE * x, opts[:top] + ABILITY_SCALE * y] })
+            pdf.fill_color "00ff00"
+            pdf.fill_and_stroke
+
+            horizontal_offset -= DIE_BUFFER
+          end
         end
 
         def draw_skill_line(pdf, vertical_offset, opts)
